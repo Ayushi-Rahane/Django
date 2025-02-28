b5c1ad181f0435707f3bdaf3cff7a38865617c4f
Ayushi-Rahane <ayushirahane2021@gmail.com>
2025-02-28 15:33:00 +0530

    c

--- userproject/home/views.py
+++ userproject/home/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
 # Create your views here.
+#PAssword: Ayushi
+def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
+    return render(request,'index.html')
+
+def login(request):
+    if request.method=='POST':
+        username =request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password = password)
+        if user is not None:
+            return redirect("/")
+        else:
+            return render(request,'login.html')
+    
+    return render(request,'login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('/login')
